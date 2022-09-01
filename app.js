690d9e93d1b93a7a9583303fb78ab8f654a89a69
Cristiano <cristianorneto@gmail.com>
2022-09-01 20:10:43 -0300

    Events & Methods

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add() {
+      this.counter++
+    },
+    reduce() {
+      this.counter--
+    }
+  }
 });
 
 app.mount('#events');
