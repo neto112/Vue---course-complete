67607634c00fe80397c485b2a2b936fd93cf37ed
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-09-01 16:28:12 -0300

    Understanding Event Binding

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finish the course and learn Vue!',
-      vueLink: 'https://vuejs.org/'
-    }
+      counter: 0,
+    };
   },
-  methods: {
-    outputGoal() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return 'Learn Vue!';
-      } else {
-        return 'Master Vue!'
-      }
-    }
-  }
 });
 
-app.mount('#user-goal');
+app.mount('#events');
