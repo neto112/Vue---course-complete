36153d1177a721f22616cefeab82fe2b1e0b123e
Cristiano <cristianorneto@gmail.com>
2022-09-05 22:05:02 -0300

    Exploring Event Modifiers

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
+      confirmedName: ''
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      event.preventDefault();
+      alert('Submitted!')
+    },
     setName(event, lastName) {
       this.name = event.target.value + ' ' + lastName
     },
