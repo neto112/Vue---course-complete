acce3fe00fc021250153d6b803b42d5207682f3c
Cristiano <cristianorneto@gmail.com>
2022-09-05 21:33:53 -0300

    Exploring Event Modifiers

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
   },
   methods: {
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName
+    },
     add(num) {
       this.counter = this.counter +num;
     },
