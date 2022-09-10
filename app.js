aa84c826f774f013d33a30361832e66c43d08db3
Cristiano <cristianorneto@gmail.com>
2022-09-10 20:04:15 -0300

    Dynamic Styling with Inline Styles

--- app.js
+++ app.js
@@ -1,66 +1,22 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 10,
-      name: '',
-      lastName: '',
-      // fullname: '',
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
     };
   },
-  watch: {
-    counter(value) {
-      if (value > 50) {
-        const that = this;
-        setTimeout(function () {
-          that.counter = 0;
-        }, 2000)
-      }
-    }
-  //   name(value) {
-  //     if (value === '') {
-  //       return ''
-  //     } else
-  //     this.fullname = value + ' ' + this.lastName;
-  //   },
-  //   lastName(value) {
-  //     if (value === '') {
-  //       return ''
-  //     } else
-  //     this.fullname = this.name + ' ' + value
-  //   }
-  },
-  computed: {
-    fullname() {
-      if (this.name === '' || this.lastName === '') {
-        return '';
-      }
-      return this.name + ' ' + this.lastName
-    }
-  },
   methods: {
-    outputFullname() {
-      if (this.name === '') {
-        return '';
+    boxSelected(box) {
+      if (box === 'A') {
+        this.boxASelected = !this.boxASelected;
+      } else if (box === 'B') {
+        this.boxBSelected = !this.boxBSelected;
+      } else if (box === 'C') {
+        this.boxCSelected = !this.boxCSelected;
       }
-      return this.name + ' ' + ' Rasweiler '
-    },
-    submitForm(event) {
-      event.preventDefault();
-      alert('Submitted!')
-    },
-    setName(event) {
-      this.name = event.target.value;
-    },
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      this.counter = this.counter - num;
-    },
-    resetInput() {
-      this.name = "";
     }
   }
 });
 
-app.mount('#events');
+app.mount('#styling');
