eb4c6262d16845136480bfbaab975dbc2e198ddb
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-09-12 10:11:12 -0300

    Rendering Content Conditionally

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      enteredGoalValue: '',
+      goals: [],
+    };
   },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue)
+    }
+  }
 });
 
 app.mount('#user-goals');
