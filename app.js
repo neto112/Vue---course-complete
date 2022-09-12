8dab2e3ef3de7e169049dcaeaf0e34df57390b5a
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-09-12 09:57:17 -0300

    Rendering Conditional Content & List

--- app.js
+++ app.js
@@ -1,33 +1,7 @@
 const app = Vue.createApp({
   data() {
-    return {
-      boxASelected: false,
-      boxBSelected: false,
-      boxCSelected: false,
-    };
-  },
-  computed: {
-    boxAClasses() {
-      return { active: this.boxASelected }
-    },
-    boxBClasses() {
-      return { active: this.boxBSelected }
-    },
-    boxCClasses() {
-      return { active: this.boxCSelected }
-    }
-  },
-  methods: {
-    boxSelected(box) {
-      if (box === 'A') {
-        this.boxASelected = !this.boxASelected;
-      } else if (box === 'B') {
-        this.boxBSelected = !this.boxBSelected;
-      } else if (box === 'C') {
-        this.boxCSelected = !this.boxCSelected;
-      }
-    }
+    return { goals: [] };
   },
 });
 
-app.mount('#styling');
+app.mount('#user-goals');
