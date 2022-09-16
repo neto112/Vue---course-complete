a5b3c2a896bfe45cb5cbfec31287cf81e385a646
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-09-16 17:46:09 -0300

    Finishing the Core Functinallity

--- app.js
+++ app.js
@@ -8,14 +8,20 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
-      winner: null
+      winner: null,
     };
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: '0%' }
+      }
       return { width: this.monsterHealth + '%' };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: '0%' }
+      }
       return { width: this.playerHealth + '%' }
     },
     myUseSpecialAttack() {
@@ -39,6 +45,12 @@ const app = Vue.createApp({
     }
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12)
@@ -64,6 +76,9 @@ const app = Vue.createApp({
         this.playerHealth += healValue
       }
       this.attackPlayer();
+    },
+    surrender() {
+      this.winner = 'monster'
     }
   }
 });
