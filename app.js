be874bdb80b9e2d3bf3e7eee0c831a42fe7fbb9b
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-09-19 15:33:22 -0300

    Introducing Components

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: 'manuel',
@@ -18,11 +17,37 @@ const app = Vue.createApp({
       ],
     };
   },
+});
+
+app.component('friend-contact', {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">
+      {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenzo',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com',
+      },
+    };
+  },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
-    }
-  }
+    },
+  },
 });
 
 app.mount('#app');
