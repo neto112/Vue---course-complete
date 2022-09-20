da609963d96cb62a1c48f199c4ee47d86ed26ded
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-09-20 12:27:36 -0300

    Creating a Basic Vue App and Adding a Component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue'
+import FriendContact from './components/FriendContact.vue'
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.component('friend-contact', FriendContact);
+
+app.mount('#app');
