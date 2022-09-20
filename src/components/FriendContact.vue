29983c00cd65c3efb228dbd6623112eff0fb7c5f
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-09-20 15:11:05 -0300

    Validating Props

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -20,7 +20,29 @@
 
 <script>
 export default {
-  props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+      default: '0',
+      validator: function(value) {
+        return value === '1' || value === '0'
+      }
+    },
+  },
   data() {
     return {
       detailsAreVisible: false,
