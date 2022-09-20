0d87daea739e24b14207e5006c2e30ee100810e5
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-09-20 17:45:13 -0300

    Demo: Adding Components & Connecting Them

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -33,7 +34,7 @@ export default {
         {
           id: "julie",
           name: "Julie Jones",
-          phone: "0123 45678 90",
+          phone: "0987 65432 10",
           email: "julie@localhost.com",
           isFavorite: false,
         },
@@ -46,6 +47,16 @@ export default {
         (friend) => friend.id === friendId
       );
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false,
+      };
+      this.friends.push(newFriendContact);
     },
   },
 };
@@ -77,7 +88,8 @@ header {
   padding: 0;
   list-style: none;
 }
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -106,5 +118,18 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -47,6 +47,7 @@ export default {
       // }
     },
   },
+  emits: ['toggle-favorite'],
   data() {
     return {
       detailsAreVisible: false,
