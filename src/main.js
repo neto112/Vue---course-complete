352a18775d35605174dffb01c4b62451deadf781
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-09-21 10:57:15 -0300

    Time to Practice: Props & Custom Events

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue'
-import FriendContact from './components/FriendContact.vue'
-import NewFriend from './components/NewFriend.vue'
+import ActiveUser from './components/ActiveUser.vue'
+import UserData from './components/UserData.vue'
 
 const app = createApp(App);
 
-app.component('friend-contact', FriendContact);
-app.component('new-friend', NewFriend);
+app.component('active-user', ActiveUser);
+app.component('user-data', UserData);
 
-app.mount('#app');
+app.mount('#app')
