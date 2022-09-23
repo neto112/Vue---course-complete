8adbed98f59d9f73d4e468418dc6862741244ea2
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-09-23 10:41:55 -0300

    A Potential Problem

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <active-user :username="item.name" :age="item.age" />
-    <user-data @set-item="setItem" />
+    <active-element
+      :topic-title="activeTopic && activeTopic.title"
+      :text="activeTopic && activeTopic.fullText"
+    ></active-element>
+    <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
   </div>
 </template>
 
@@ -9,19 +12,89 @@
 export default {
   data() {
     return {
-      item: {
-        name: "Cristiano Rasweiler Neto",
-        age: 27,
-      },
+      topics: [
+        {
+          id: 'basics',
+          title: 'The Basics',
+          description: 'Core Vue basics you have to know',
+          fullText:
+            'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
+        },
+        {
+          id: 'components',
+          title: 'Components',
+          description:
+            'Components are a core concept for building Vue UIs and apps',
+          fullText:
+            'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
+        },
+      ],
+      activeTopic: null,
     };
   },
   methods: {
-    setItem(name, age) {
-      this.item = {
-        name: name,
-        age: age,
-      };
+    activateTopic(topicId) {
+      this.activeTopic = this.topics.find((topic) => topic.id === topicId);
     },
   },
 };
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+section {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 2rem auto;
+  max-width: 40rem;
+  padding: 1rem;
+  border-radius: 12px;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+}
+
+li {
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  width: 15rem;
+  margin: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+h2 {
+  margin: 0.75rem 0;
+  text-align: center;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #c70053;
+  background-color: #c70053;
+  color: white;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #e24d8b;
+  border-color: #e24d8b;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue';
-import App from './App.vue'
-import ActiveUser from './components/ActiveUser.vue'
-import UserData from './components/UserData.vue'
+
+import App from './App.vue';
+import ActiveElement from './components/ActiveElement.vue';
+import KnowledgeBase from './components/KnowledgeBase.vue';
+import KnowledgeElement from './components/KnowledgeElement.vue';
+import KnowledgeGrid from './components/KnowledgeGrid.vue';
 
 const app = createApp(App);
 
-app.component('active-user', ActiveUser);
-app.component('user-data', UserData);
+app.component('active-element', ActiveElement);
+app.component('knowledge-base', KnowledgeBase);
+app.component('knowledge-element', KnowledgeElement);
+app.component('knowledge-grid', KnowledgeGrid);
 
-app.mount('#app')
+app.mount('#app');
