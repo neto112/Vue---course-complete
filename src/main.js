b1b604eb1a0e043af33d339df6f7bde3d5b0594a
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-10-03 15:06:32 -0300

    Scoped Slots

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import ActiveElement from './components/ActiveElement.vue';
-import KnowledgeBase from './components/KnowledgeBase.vue';
-import KnowledgeElement from './components/KnowledgeElement.vue';
-import KnowledgeGrid from './components/KnowledgeGrid.vue';
+import BaseBadge from './components/BaseBadge.vue';
+import BaseCard from './components/BaseCard.vue';
 
 const app = createApp(App);
 
-app.component('active-element', ActiveElement);
-app.component('knowledge-base', KnowledgeBase);
-app.component('knowledge-element', KnowledgeElement);
-app.component('knowledge-grid', KnowledgeGrid);
+app.component('base-card', BaseCard);
+app.component('base-badge', BaseBadge);
 
 app.mount('#app');
