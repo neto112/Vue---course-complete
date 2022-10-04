9384ffb209eff1b9cc496b8ead45016bebfff2d1
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-10-04 10:23:46 -0300

    The Learning Resources App

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,39 @@
 <template>
-  <div>
-    <TheHeader />
-    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
-    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <!-- <ActiveGoals v-if="selectedComponent === 'active-goals'" /> -->
-    <!-- <ManageGoals v-if="selectedComponent === 'manage-goals'"/> -->
-    <keep-alive>
-      <component :is="selectedComponent"></component>
-    </keep-alive>
-  </div>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
 </template>
 
 <script>
-import TheHeader from "./components/layout/TheHeader.vue";
-import ActiveGoals from "./components/ActiveGoals.vue";
-import ManageGoals from "./components/ManageGoals.vue";
+import LearningResource from './components/learning-resources/LearningResource.vue';
 
 export default {
   components: {
-    TheHeader,
-    ActiveGoals,
-    ManageGoals,
+    LearningResource,
   },
   data() {
     return {
-      selectedComponent: 'active-goals',
-      activeUser: {
-        name: "Maximilian Schwarzmüller",
-        description: "Site owner and admin",
-        role: "admin",
-      },
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
     };
   },
-  methods: {
-    setSelectedComponent(cmp) {
-      this.selectedComponent = cmp
-    }
-  }
 };
 </script>
-
-<style>
-html {
-  font-family: sans-serif;
-}
-
-body {
-  margin: 0;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import BaseBadge from './components/UI/BaseBadge.vue';
-import BaseCard from './components/UI/BaseCard.vue';
 
-const app = createApp(App);
-
-app.component('base-card', BaseCard);
-app.component('base-badge', BaseBadge);
-
-app.mount('#app');
+createApp(App).mount('#app');
