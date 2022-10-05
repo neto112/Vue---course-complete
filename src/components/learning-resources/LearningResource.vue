49d88622b7c94dc6825b084836af15bc4f2052e3
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-10-05 12:01:38 -0300

    Dynamic Components & Attribute Fallthrough

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <base-button mode="flat">Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
 
 const app = createApp(App)
 
 app.component('base-card', BaseCard)
+app.component('base-button', BaseButton)
 app.mount('#app');
