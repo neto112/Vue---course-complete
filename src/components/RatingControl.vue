51dd739e556b703d4f51f11651717bf6f7fda293
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-10-11 14:29:17 -0300

    Using v-model on Custom Components

--- src/components/RatingControl.vue
+++ src/components/RatingControl.vue
@@ -1,12 +1,12 @@
 <template>
   <ul>
-    <li :class="{active: activeOption === 'poor'}">
+    <li :class="{active: modelValue === 'poor'}">
       <button type="button" @click="activate('poor')">Poor</button>
     </li>
-    <li :class="{active: activeOption === 'average'}">
+    <li :class="{active: modelValue === 'average'}">
       <button type="button" @click="activate('average')">Average</button>
     </li>
-    <li :class="{active: activeOption === 'great'}">
+    <li :class="{active: modelValue === 'great'}">
       <button type="button" @click="activate('great')">Great</button>
     </li>
   </ul>
@@ -14,14 +14,21 @@
 
 <script>
 export default {
-  data() {
-    return {
-      activeOption: null,
-    };
-  },
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  // data() {
+  //   return {
+  //     activeOption: this.modelValue,
+  //   };
+  // },
+  // computed: {
+  //   activeOption() {
+  //     return this.modelValue;
+  //   }
+  // },
   methods: {
     activate(option) {
-      this.activeOption = option;
+      this.$emit('update:modelValue', option);
     },
   },
 };
--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -60,7 +60,7 @@
       </div>
     </div>
     <div class="form-control">
-      <rating-control></rating-control>
+      <rating-control v-model="rating"></rating-control>
     </div>
     <div class="form-control">
       <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm" />
@@ -87,6 +87,7 @@ export default {
       interest: [],
       how: null,
       confirm: false,
+      rating: null,
       userNameValidity: 'pending'
     };
   },
@@ -110,6 +111,9 @@ export default {
       console.log('Confirm?');
       console.log(this.confirm);
       this.confirm = false;
+      console.log('Rating');
+      console.log(this.rating);
+      this.rating = null;
     },
     validateInput() {
       if (this.userName === '') {
