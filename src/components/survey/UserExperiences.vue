9f0fe86dfa1f9775002472d0df554a6ec361b4e2
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-10-14 09:02:16 -0300

    Getting Data (GET Request) & Transofrming Response Data

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -18,12 +20,39 @@
 </template>
 
 <script>
-import SurveyResult from './SurveyResult.vue';
+import SurveyResult from "./SurveyResult.vue";
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch(
+        "https://vue-http-demo-64154-default-rtdb.firebaseio.com/surveys.json"
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        });
+    },
   },
 };
 </script>
