28e2b53bd912a4d08ac474777677106899b93910
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-10-18 11:54:22 -0300

    Navigation & Dynamic Paths

--- src/components/teams/TeamsItem.vue
+++ src/components/teams/TeamsItem.vue
@@ -2,13 +2,18 @@
   <li>
     <h3>{{ name }}</h3>
     <div class="team-members">{{ memberCount }} Members</div>
-    <a href="#">View Members</a>
+    <router-link :to="teamMembersLink">View Members</router-link>
   </li>
 </template>
 
 <script>
 export default {
-  props: ['name', 'memberCount'],
+  props: ['id', 'name', 'memberCount'],
+  computed: {
+    teamMembersLink() {
+      return '/teams/' + this.id
+    }
+  }
 };
 </script>
 
--- src/components/teams/TeamsList.vue
+++ src/components/teams/TeamsList.vue
@@ -3,6 +3,7 @@
     <teams-item
       v-for="team in teams"
       :key="team.id"
+      :id="team.id"
       :name="team.name"
       :member-count="team.members.length"
     ></teams-item>
