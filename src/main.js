c2b44249f5a4c06184e6a565a9ec841c551ec073
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-10-18 09:59:53 -0300

    Registering & Rendering Routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from "vue-router";
 
 import App from './App.vue';
+import TeamsList from "./components/teams/TeamsList.vue";
+import UsersList from "./components/teams/UsersList.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList },
+  ]
+});
 
 const app = createApp(App)
+
+app.use(router)
 
 app.mount('#app');
