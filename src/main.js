b8754ee08f9508a8c34010c00fdaa207899593d3
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-10-25 17:30:06 -0300

    Using Nested Routes

--- src/main.js
+++ src/main.js
@@ -11,9 +11,15 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
-    { path: '/teams', component: TeamsList },
+    {
+      name: 'teams',
+      path: '/teams',
+      component: TeamsList,
+      children: [
+        { name: 'team-members', path: ':teamId', component: TeamMembers, props: true }
+      ]
+    },
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true },
     { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active',
