11bfdcac22208941c8c576756a8190f49230a453
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-11-09 09:32:16 -0300

    Introducing Navigation Guards

--- src/main.js
+++ src/main.js
@@ -36,14 +36,24 @@ const router = createRouter({
     { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active',
-  scrollBehavior(to, from, savedPosition) {
-    console.log(to, from, savedPosition);
+  scrollBehavior(_, _2, savedPosition) {
     if (savedPosition) {
       return savedPosition;
     }
     return { left: 0, top: 0 };
   }
 });
+
+router.beforeEach((to, from, next) => {
+  console.log('Global beforeEach');
+  console.log(to, "***", from);
+  // if (to.name === 'team-members') {
+  //   ne xt();
+  // } else {
+  //   next({ name: 'team-members', params: { teamId: 't2' } })
+  // }
+    next();
+})
 
 const app = createApp(App)
 
