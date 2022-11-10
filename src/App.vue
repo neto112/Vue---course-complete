1579eb2621d58d3e59fd5617768810393f226086
cristianorneto@gmail.com <cristianorneto@gmail.com>
2022-11-10 14:11:53 -0300

    Using the Transition Component

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    <transition>
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    </transition>
     <button @click="toggleParagraph">Toogle Paragraph</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
@@ -87,6 +89,34 @@ button:active {
 .animate {
   /* transform: translateX(-150px); */
   animation: slide-fade 0.3s ease-out forwards;
+}
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active {
+  transition: all 0.9s ease-out;
+}
+
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-from {
+  opacity: 1;
+  transform: translateY(0px);
+}
+
+.v-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 
 @keyframes slide-fade {
