220b7355d5529ab246a5b3551e56f51785e5a1f9
cristianorneto@gmail.com <cristianorneto@gmail.com>
2023-01-09 09:11:28 -0300

    Main Project: "Find a Coach" Web App

--- src/main.js
+++ src/main.js
@@ -1,15 +1,3 @@
 import { createApp } from 'vue';
 
-import router from './router.js';
-import store from './store/index.js';
-import App from './App.vue';
-import BaseBadge from './components/ui/BaseBadge.vue';
-
-const app = createApp(App)
-
-app.use(router);
-app.use(store);
-
-app.component('base-badge', BaseBadge);
-
-app.mount('#app');
+createApp({}).mount('#app');
