6921b30a8e44b17a50abeb8864f27568bf27df1d
cristianorneto@gmail.com <cristianorneto@gmail.com>
2023-01-17 08:38:47 -0300

    Registering as a Coach: The Form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,18 @@
 <template>
-  REGISTER
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
