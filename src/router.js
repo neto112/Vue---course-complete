8875459c848f223ec3a46cab1172de9281e9aaf3
cristianorneto@gmail.com <cristianorneto@gmail.com>
2023-01-17 08:26:51 -0300

    router

--- src/router.js
+++ src/router.js
@@ -15,6 +15,7 @@ const router = createRouter({
     {
       path: '/coaches/:id',
       component: CoachDetail,
+      props: true,
       children: [
         { path: 'contact', component: ContactCoach } // /coaches/c1/contact
       ]
