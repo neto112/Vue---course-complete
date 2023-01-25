9d887930c1d61922afc055eb13701aefe612d02c
cristianorneto@gmail.com <cristianorneto@gmail.com>
2023-01-25 11:25:22 -0300

    Preparing Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,22 +2,14 @@ import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
 import requestsModule from './modules/requests/index.js';
+import authModule from './modules/auth/index.js'
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
-    requests: requestsModule
+    requests: requestsModule,
+    auth: authModule,
   },
-  state() {
-    return {
-      userId: 'c3'
-    };
-  },
-  getters: {
-    userId(state) {
-      return state.userId;
-    }
-  }
 });
 
 export default store;
---
+++ src/store/modules/auth/index.js
@@ -0,0 +1,14 @@
+import mutations from "./mutations.js"
+import actions from "./actions.js"
+import getters from "./getters.js"
+
+export default {
+  state() {
+    return {
+      userId: 'c3'
+    };
+  },
+  mutations,
+  actions,
+  getters,
+}
