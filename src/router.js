4fd2400f5e19cf2d0883134b682e1cec8ece3a5a
cristianorneto@gmail.com <cristianorneto@gmail.com>
2023-01-25 11:11:41 -0300

    Adding an Authentication Page (Login & Signup)

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import CoachRegistation from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -22,6 +23,7 @@ const router = createRouter({
     },
     { path: '/register', component: CoachRegistation },
     { path: '/requests', component: RequestsReceived },
+    { path: '/auth', component: UserAuth },
     { path: '/:notFound(.*)', component: NotFound }
   ]
 });
