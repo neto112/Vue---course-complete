f2c6bed8a6661b45feff4ac59ddb69bdf73da297
cristianorneto@gmail.com <cristianorneto@gmail.com>
2023-02-03 17:11:35 -0300

    Adding a "Login" Action & Flow

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -9,11 +9,14 @@
         <label for="password">Password</label>
         <input type="password" id="password" v-model.trim="password" />
       </div>
-      <p
-        v-if="!formIsValid"
-      >Please enter a valid email and password (must be at least 6 characters long).</p>
+      <p v-if="!formIsValid">
+        Please enter a valid email and password (must be at least 6 characters
+        long).
+      </p>
       <base-button>{{ submitButtonCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
+      <base-button type="button" mode="flat" @click="switchAuthMode">{{
+        switchModeButtonCaption
+      }}</base-button>
     </form>
   </base-card>
 </template>
@@ -22,54 +25,56 @@
 export default {
   data() {
     return {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       formIsValid: true,
-      mode: 'login',
+      mode: "login",
     };
   },
   computed: {
     submitButtonCaption() {
-      if (this.mode === 'login') {
-        return 'Login';
+      if (this.mode === "login") {
+        return "Login";
       } else {
-        return 'Signup';
+        return "Signup";
       }
     },
     switchModeButtonCaption() {
-      if (this.mode === 'login') {
-        return 'Signup instead';
+      if (this.mode === "login") {
+        return "Signup instead";
       } else {
-        return 'Login instead';
+        return "Login instead";
       }
     },
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       this.formIsValid = true;
       if (
-        this.email === '' ||
-        !this.email.includes('@') ||
+        this.email === "" ||
+        !this.email.includes("@") ||
         this.password.length < 6
       ) {
         this.formIsValid = false;
         return;
       }
 
-      if (this.mode === 'login') {
-        // ...
+      const actionPayload = {
+        email: this.email,
+        password: this.password,
+      };
+
+      if (this.mode === "login") {
+        await this.$store.dispatch("login", actionPayload);
       } else {
-        this.$store.dispatch('signup', {
-          email: this.email,
-          password: this.password,
-        });
+        this.$store.dispatch("signup", actionPayload);
       }
     },
     switchAuthMode() {
-      if (this.mode === 'login') {
-        this.mode = 'signup';
+      if (this.mode === "login") {
+        this.mode = "signup";
       } else {
-        this.mode = 'login';
+        this.mode = "login";
       }
     },
   },
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,29 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBd54EneJ3mjFGWWcMF4kprUvD0luHPiBo', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn
+    });
+  },
   async signup(context, payload) {
     const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBd54EneJ3mjFGWWcMF4kprUvD0luHPiBo', {
       method: 'POST',
