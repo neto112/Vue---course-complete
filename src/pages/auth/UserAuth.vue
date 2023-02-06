97200d9ab6a47642e3ebffd27bcbab49a629d774
cristianorneto@gmail.com <cristianorneto@gmail.com>
2023-02-06 08:46:46 -0300

    Better UX: Loading Spinner & Error Handling

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,49 +1,56 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">E-Mail</label>
-        <input type="email" id="email" v-model.trim="email" />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model.trim="password" />
-      </div>
-      <p v-if="!formIsValid">
-        Please enter a valid email and password (must be at least 6 characters
-        long).
-      </p>
-      <base-button>{{ submitButtonCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthMode">{{
-        switchModeButtonCaption
-      }}</base-button>
-    </form>
-  </base-card>
+  <div>
+    <base-dialog :show="!!error" title="An error occurred" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">E-Mail</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p
+          v-if="!formIsValid"
+        >Please enter a valid email and password (must be at least 6 characters long).</p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
       formIsValid: true,
-      mode: "login",
+      mode: 'login',
+      isLoading: false,
+      error: null,
     };
   },
   computed: {
     submitButtonCaption() {
-      if (this.mode === "login") {
-        return "Login";
+      if (this.mode === 'login') {
+        return 'Login';
       } else {
-        return "Signup";
+        return 'Signup';
       }
     },
     switchModeButtonCaption() {
-      if (this.mode === "login") {
-        return "Signup instead";
+      if (this.mode === 'login') {
+        return 'Signup instead';
       } else {
-        return "Login instead";
+        return 'Login instead';
       }
     },
   },
@@ -51,31 +58,42 @@ export default {
     async submitForm() {
       this.formIsValid = true;
       if (
-        this.email === "" ||
-        !this.email.includes("@") ||
+        this.email === '' ||
+        !this.email.includes('@') ||
         this.password.length < 6
       ) {
         this.formIsValid = false;
         return;
       }
 
+      this.isLoading = true;
+
       const actionPayload = {
         email: this.email,
         password: this.password,
       };
 
-      if (this.mode === "login") {
-        await this.$store.dispatch("login", actionPayload);
-      } else {
-        this.$store.dispatch("signup", actionPayload);
+      try {
+        if (this.mode === 'login') {
+          await this.$store.dispatch('login', actionPayload);
+        } else {
+          await this.$store.dispatch('signup', actionPayload);
+        }
+      } catch (err) {
+        this.error = err.message || 'Failed to authenticate, try later.';
       }
+
+      this.isLoading = false;
     },
     switchAuthMode() {
-      if (this.mode === "login") {
-        this.mode = "signup";
+      if (this.mode === 'login') {
+        this.mode = 'signup';
       } else {
-        this.mode = "login";
+        this.mode = 'login';
       }
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
