4b3465a1bae65edbc8e4499645c2b62fb89cf459
cristianorneto@gmail.com <cristianorneto@gmail.com>
2023-02-22 09:48:04 -0300

    Migating the Remaining Components

--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -13,6 +13,8 @@
 </template>
 
 <script>
+import { ref, computed, watch, toRefs } from 'vue';
+
 import ProjectItem from './ProjectItem.vue';
 
 export default {
@@ -20,42 +22,87 @@ export default {
     ProjectItem,
   },
   props: ['user'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-    };
-  },
-  computed: {
-    hasProjects() {
-      return this.user.projects && this.availableProjects.length > 0;
-    },
-    availableProjects() {
-      if (this.activeSearchTerm) {
-        return this.user.projects.filter((prj) =>
-          prj.title.includes(this.activeSearchTerm)
+  setup(props) {
+    const enteredSearchTerm = ref('');
+    const activeSearchTerm = ref('');
+
+    const availableProjects = computed(function () {
+      if (activeSearchTerm.value) {
+        return props.user.projects.filter((prj) =>
+          prj.title.includes(activeSearchTerm.value)
         );
       }
-      return this.user.projects;
-    },
-  },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-  },
-  watch: {
-    enteredSearchTerm(val) {
+      return props.user.projects;
+    });
+
+    const hasProjects = computed(function () {
+      return props.user.projects && availableProjects.value.length > 0;
+    });
+
+    watch(enteredSearchTerm, function (newValue) {
       setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
+        if (newValue === enteredSearchTerm.value) {
+          activeSearchTerm.value = newValue;
         }
       }, 300);
-    },
-    user() {
-      this.enteredSearchTerm = '';
-    },
+    });
+
+    // const propsWithRefs = toRefs(props);
+    // const user = propsWithRefs.user;
+
+    const { user } = toRefs(props);
+
+    watch(user, function () {
+      enteredSearchTerm.value = '';
+    });
+
+    function updateSearch(val) {
+      enteredSearchTerm.value = val;
+    }
+
+    return {
+      enteredSearchTerm,
+      availableProjects,
+      hasProjects,
+      updateSearch,
+    };
   },
+  // data() {
+  //   return {
+  //     enteredSearchTerm: '',
+  //     activeSearchTerm: '',
+  //   };
+  // },
+  // computed: {
+  //   hasProjects() {
+  //     return this.user.projects && this.availableProjects.length > 0;
+  //   },
+  //   availableProjects() {
+  //     if (this.activeSearchTerm) {
+  //       return this.user.projects.filter((prj) =>
+  //         prj.title.includes(this.activeSearchTerm)
+  //       );
+  //     }
+  //     return this.user.projects;
+  //   },
+  // },
+  // methods: {
+  //   updateSearch(val) {
+  //     this.enteredSearchTerm = val;
+  //   },
+  // },
+  // watch: {
+  //   enteredSearchTerm(val) {
+  //     setTimeout(() => {
+  //       if (val === this.enteredSearchTerm) {
+  //         this.activeSearchTerm = val;
+  //       }
+  //     }, 300);
+  //   },
+  //   user() {
+  //     this.enteredSearchTerm = '';
+  //   },
+  // },
 };
 </script>
 
